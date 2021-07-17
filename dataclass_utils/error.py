f19343c62c4bb2f99c098a0cfea1db4982e883c0
Yohei Tamura <tamuhey@gmail.com>
2021-07-17 15:26:00 +0900

    fix error msg

--- dataclass_utils/error.py
+++ dataclass_utils/error.py
@@ -39,7 +39,7 @@ class MissingKeyError(Error):
 
     def __str__(self):
         path = _path_to_str(self.path)
-        return f"Key Error in '{path}': Got '{self.key}' in 'self.value' but doesn't exist in '{self.ty}'"
+        return f"Key Error in '{path}': Got '{self.key}' in '{self.value}' but doesn't exist in '{self.ty}'"
 
 
 class UnsupportedTypeError(Error):
