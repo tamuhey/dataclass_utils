81580d2c0c285081db06be02b4ecdd125532bef5
Yohei Tamura <tamuhey@gmail.com>
2022-08-30 21:00:50 +0900

    fix test

--- tests/test_pep604.py
+++ tests/test_pep604.py
@@ -6,18 +6,17 @@ import dataclass_utils
 
 if sys.version_info < (3, 10):
     pytestmark = pytest.mark.skip()
+else:
+    @dataclasses.dataclass
+    class Foo:
+        bar: str | int = "foo"
 
 
-@dataclasses.dataclass
-class Foo:
-    bar: str | int = "foo"
+    def test_union():
+        check_type(Foo())
+        with pytest.raises(TypeError):
+            check_type(Foo({}))
 
 
-def test_union():
-    check_type(Foo())
-    with pytest.raises(TypeError):
-        check_type(Foo({}))
-
-
-def test_union2():
-    dataclass_utils.into({"bar": "foo"}, Foo)
+    def test_union2():
+        dataclass_utils.into({"bar": "foo"}, Foo)
